ed4ffc18c6866b12558836382be209a895cebb49
HIMANSHU SINGH <200160223002.himanshu@gdgu.org>
2023-07-31 23:06:56 +0530

    Add files via upload

---
+++ src/app/login/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const page = () => {
+  const session = useSession();
+  console.log(session);
+  if (session.status === "loading") return <p>Loading...</p>;
+  if (session.status === "authenticated")
+    return (
+      <div>
+        <p>Authenticated</p>
+        <button onClick={() => signOut("google")}>Logout</button>
+      </div>
+    );
+  if (session.status === "unauthenticated")
+    return <button onClick={() => signIn("google")}>Login With google</button>;
+
+  
+  return <div>Login Page</div>;
+};
+
+export default page;
